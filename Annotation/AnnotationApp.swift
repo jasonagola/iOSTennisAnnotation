59174d0b3bf37765cf473c9a00aef1ddab72b5bc
jasonagola <jason.agola@gmail.com>
2025-01-08 15:59:59 -0500

    Initial Commit

---
+++ Annotation/AnnotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnnotationApp.swift
+//  Annotation
+//
+//  Created by Jason Agola on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AnnotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnnotationTests/AnnotationTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnnotationTests.swift
+//  AnnotationTests
+//
+//  Created by Jason Agola on 1/8/25.
+//
+
+import Testing
+@testable import Annotation
+
+struct AnnotationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
